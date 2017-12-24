f0d3d2542516b6779c5bfd7359a96f80cb7df59f
Johannes Hoppe <info@johanneshoppe.com>
2017-12-24 19:59:43 +0000

    Add more tests

---
+++ tests/commands/test_create_development_schema.py
@@ -0,0 +1,28 @@
+from io import StringIO
+
+import pytest
+from django.core.management import CommandError, call_command
+from django.db import connection
+
+
+class TestCreateDevelopmentSchema:
+
+    def test_default(self, db):
+        with connection.cursor() as c:
+            c.execute('DROP SCHEMA salesforce CASCADE;')
+        with StringIO() as sql:
+            call_command('create_development_schema', stdout=sql)
+            sql.seek(0)
+            assert sql.read() == 'Schema salesforce created.\n'
+
+    def test_exception(self, db):
+        with pytest.raises(CommandError) as e:
+            call_command('create_development_schema')
+
+        assert 'Schema salesforce already exists.' in str(e)
+
+    def test_force(self, db):
+        with StringIO() as sql:
+            call_command('create_development_schema', '--force', stdout=sql)
+            sql.seek(0)
+            assert sql.read() == 'Schema salesforce created.\n'
