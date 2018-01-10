6b8602fd83db897573381b56c562c5dca8b61a75
Ankush Chadda <ankush.chadda@thermondo.de>
2018-01-10 15:46:18 +0100

    fix lint issues

--- tests/contrib/test_healthcheck.py
+++ tests/contrib/test_healthcheck.py
@@ -1,6 +1,7 @@
 from heroku_connect.contrib.health_check import HerokuConnectHealthCheck
 from heroku_connect.test.utils import heroku_cli
 
+
 def test_healthcheck_connection():
     success_output = """Connection [7976d2d4-2483 / herokuconnect-asymmetrical] (IDLE)
            --> Contact (DATA_SYNCED)"""
