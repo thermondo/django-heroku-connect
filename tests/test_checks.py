385925ca47afd6543f0ef0f7278f6047065e8fa6
Ankush Chadda <ankush.chadda@thermondo.de>
2018-01-10 14:20:32 +0100

    add text changes to test as well

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -10,7 +10,7 @@ from heroku_connect.checks import _check_foreign_key_target
 def test_check_foreign_key_target():
     errors = _check_foreign_key_target(apps)
     assert errors == [checks.Error(
-        "testapp.OtherModel.number points to an External ID or the 'sfid', not 'id'.",
+        "testapp.OtherModel.number should point to an External ID or the 'sf_id', not 'id'.",
         hint="Specify the 'to_field' argument.",
         id='heroku_connect.E005',
     )]
