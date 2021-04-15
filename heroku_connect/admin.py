15835017c30640737c7bff5e3f5f41c4a8408cef
Rust Saiargaliev <rustem.saiargaliev@thermondo.de>
2021-04-15 13:17:16 +0200

    Replace deprecated Django ugettext_lazy
    with `gettext_lazy`
    
    ```
    RemovedInDjango40Warning: django.utils.translation.ugettext_lazy() is deprecated in favor of django.utils.translation.gettext_lazy().
    ```

--- heroku_connect/admin.py
+++ heroku_connect/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.db import transaction
 from django.urls import reverse
 from django.utils.html import format_html
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from heroku_connect.models import (
     TRIGGER_LOG_STATE, TriggerLog, TriggerLogArchive
